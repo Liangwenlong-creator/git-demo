d7dfa17d817b7d81fb181921966fb64fc162dccb
lwl <2762302179@qq.com>
2023-03-15 19:34:26 +0800

    取消test.cpp中的注释

--- c++/src/test.cpp
+++ c++/src/test.cpp
@@ -10,6 +10,6 @@ int main()
     int a=5, b=7;
     cout << "before swap: " << a << " " << b <<endl;
     swap(a, b);
-    // cout << "after swap: " << a << " " << b <<endl;
+    cout << "after swap: " << a << " " << b <<endl;
     return 0;
 }
